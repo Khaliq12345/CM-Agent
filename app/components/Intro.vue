763ad7eda3c905c76885b0d4b61b291370f3ac89
wallcreation <lanhajames@gmail.com>
2026-06-26 22:17:39 +0100

    Add motion video and setup it in intro component

--- app/components/Intro.vue
+++ app/components/Intro.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="py-4 md:py-46 grid grid-cols-1 md:grid-cols-2 between">
+  <div class="py-4 flex flex-col gap-4">
     <!-- left section -->
-    <section class="space-y-8">
+    <section class="space-y-8 md:max-w-[50%]">
       <h1 class="text-2xl md:text-[60px] font-medium">
         L'agent IA qui gère vos réseaux sociaux
       </h1>
@@ -15,7 +15,15 @@
     </section>
 
     <!-- right section -->
-    <section></section>
+    <section>
+      <video
+        src="/motion.mp4"
+        autoplay
+        loop
+        muted
+        class="w-full h-full object-cover"
+      ></video>
+    </section>
   </div>
 </template>
 
